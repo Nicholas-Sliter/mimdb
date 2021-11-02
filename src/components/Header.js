6ae522fbd72ac2b42029c8803e4f1874980be1dc
Nick McKalip <nmckalip@middlebury.edu>
2021-11-02 18:23:27 -0400

    Commit before updating branch with main

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@
 import styles from "../styles/NavBar.module.css";
 
 
-export default function Header() {
+export default function Header(searchResults, filterResults) {
+
+
 
 
 
@@ -12,8 +14,24 @@ export default function Header() {
         <div className={styles.navbar}>
             <a className={styles.logo}><img src="/mimdb-logo-full.svg" /></a>
             <div className={styles.filter}>
-                <a className={styles.filterCrit}>Genres</a>
-                <a className={styles.filterCrit}>Courses</a>
+                <div className={styles.dropdown}>
+                  <div className={styles.filterCrit}>Genres
+                      <div className={styles.dropdownContainer}>
+                        <a onClick>Fantasy</a>
+                        <a>Sci-Fi</a>
+                        <a>Documentary</a>
+                      </div>
+                  </div>
+                </div>
+                <div className={styles.dropdown}>
+                  <div className={styles.filterCrit}>Classes
+                      <div className={styles.dropdownContainer}>
+                        <a>Sight and Sound</a>
+                        <a>Documentary</a>
+                        <a>Film101</a>
+                      </div>
+                  </div>
+                </div>
                 <a className={styles.serachbar}><input type="text" placeholder="Search.."/></a>
             </div>
         </div> 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,17 @@
-
-import NavBar from "../components/NavBar"
-
-
+import { useEffect, useState } from "react";
 import CustomHead from "../components/CustomHead";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
+  const [searchResults, setSearchResults] = useState();
+  const [ filterResults, setFilterResults] = useState;
 
 
   return (
     <div className={styles.container}>
           <CustomHead/>
-          <Header/>
+          <Header searchResults={searchResults} filterResults={filterResults}/>
       <main>
         <h1 className="title">Final Project</h1>
         <p>Hooray, you got the project deployed!</p>
