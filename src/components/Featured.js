437d70c97e818cf7e63507df6babd737dd0e6d59
kkosior1120 <kkosior@middlebury.edu>
2021-11-03 09:21:04 -0400

    fixed linter and added useEffect

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Featured({ collection }) {
     const [featuredFilms, setFeatured] = useState()
@@ -8,24 +8,28 @@ export default function Featured({ collection }) {
         let random2
         let random3
 
-        while(random1 == random2 || random1 == random3 || random2 == random3) {
+        while(random1 === random2 || random1 === random3 || random2 === random3) {
             random1 = Math.randomInt(1, collection.length)
             random2 = Math.randomInt(1, collection.length)
             random3 = Math.randomInt(1, collection.length)
         }
 
-        const movie1 = collection.find(movie => movie.id == random1)
-        const movie2 = collection.find(movie => movie.id == random2)
-        const movie3 = collection.find(movie => movie.id == random3)
+        const movie1 = collection.find(movie => movie.id === +random1)
+        const movie2 = collection.find(movie => movie.id === +random2)
+        const movie3 = collection.find(movie => movie.id === +random3)
 
         setFeatured([movie1, movie2, movie3])
     }
+
+    useEffect(() => {
+        getFeatured()
+    }, [])
 
     return (
     <div>
         <div>{featuredFilms[0]}</div>
         <div>{featuredFilms[1]}</div>
-        <div>{featuredFilms[1]}</div>
+        <div>{featuredFilms[2]}</div>
     </div>
     )
 }
