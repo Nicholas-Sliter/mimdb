4be78d9e08d7f83d15fdbb1468abb37f83692b3e
Wayne Wang <sw@middlebury.edu>
2021-11-03 17:57:23 -0400

    fixing _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,10 @@
 /* eslint-disable */
 import '../styles/globals.css';
 import { useState } from "react";
-import data from "../../data/mockData.json";
 
 function MyApp({ Component, pageProps }) {
-  const [films, setFilms] = useState(data);
 
-  const props = {...pageProps, films, setFilms};
+  const props = {...pageProps};
   return <Component {...props} />;
 }
 
