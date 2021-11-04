d5301ce390405b029a8d15d2ebc3526b947dd4d7
kkosior1120 <kkosior@middlebury.edu>
2021-11-03 21:25:03 -0400

    E6

--- src/hooks/useFeatured.js
+++ src/hooks/useFeatured.js
@@ -7,9 +7,7 @@ export default function Featured({ collection }) {
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
-            const temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            [array[i], array[j]] = [array[j], array[i]];
         }
         return array
     }
