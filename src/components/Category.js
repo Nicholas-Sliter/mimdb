2652df2383d6321e3bd2e3280c275d8ede59697f
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-11-05 23:47:34 -0400

    Added prelim link to /search/ for categories

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,29 +3,33 @@ Category.js
 
 Display a category of drop down menu that is used in Header.js
 */
+import Link from "next/link";
+
 import styles from "../styles/NavBar.module.css";
 import { FiChevronDown } from "react-icons/fi";
 
+export default function Category({ fieldName, fieldList }) {
+  let uniqueFields = "";
+  if (fieldList === undefined) {
+    uniqueFields = <a />;
+  } else {
+    uniqueFields = fieldList.map((field) => (
+      <Link href="/search/" passHref key={field}>
+        <a data-testid="dropdown">
+          {field}
+        </a>
+      </Link>
+    ));
+  }
 
-export default function Category({fieldName, fieldList}){
-    let uniqueFields = "";
-    if (fieldList === undefined){
-        uniqueFields = <a/>
-    }
-    else{
-        let key = 0; //key should not be an array index, needs to change to the field name as it will be unique
-        uniqueFields = fieldList.map((field)=><a data-testid="dropdown" key={key++}>{field}</a>);
-    }
-
-
-    return (
-      <div className={styles.dropdown}>
-        <div className={`${styles.filterCrit} noselect`}>
-          <span>
-            <b>{fieldName}</b> <FiChevronDown className={styles.icon} />
-          </span>
-          <div className={styles.dropdownContainer}>{uniqueFields}</div>
-        </div>
+  return (
+    <div className={styles.dropdown}>
+      <div className={`${styles.filterCrit} noselect`}>
+        <span>
+          <b>{fieldName}</b> <FiChevronDown className={styles.icon} />
+        </span>
+        <div className={styles.dropdownContainer}>{uniqueFields}</div>
       </div>
-    );
+    </div>
+  );
 }
