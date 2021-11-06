b274dcfc50c925b89b65505651eae0be0cfc66bb
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-11-06 18:18:51 -0400

    search bar changes

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import router from "next/router";
+import styles from "../styles/SearchBar.module.scss";
+import { useState } from "react";
+import {FiSearch} from "react-icons/fi";
+
+export default function SearchBar({ placeholder = "Search ..." }) {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  //go to search page on enter
+  const handleSubmit = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      router.push(`/search?keyword=${search}`);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <FiSearch />
+      <input
+        className={styles.searchBar}
+        type="text"
+        placeholder={placeholder}
+        onChange={(e) => handleChange(e)}
+        onKeyUp={(e) => handleSubmit(e)}
+      />
+    </div>
+  );
+}
