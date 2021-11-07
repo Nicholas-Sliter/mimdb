192ac05e77520db6068990369cdb39fded2bfac2
Nick McKalip <nmckalip@middlebury.edu>
2021-11-07 17:35:01 -0500

    Fixed a linting error

--- src/components/SingleFilmDisplay.js
+++ src/components/SingleFilmDisplay.js
@@ -9,54 +9,53 @@ import styles from "../styles/SingleFilmDisplay.module.scss";
 
 
 
-export default function SingleFilmDisplay({film}) {
+export default function SingleFilmDisplay({ film }) {
 
     //quick return if undefined film
-    if (!film){
-        return(
+    if (!film) {
+        return (
             <p>Choose a Film!</p>
         );
     }
 
-    const directors = film.directors.map((director)=><li key={director}>{director}</li>);
-    const actors = film.actors.map((actor)=><li key={actor}>{actor}</li>);
-    const contributors = film.contributors.map((contrib)=><li key={contrib}>{contrib}</li>);
+    const directors = film.directors.map((director) => <li key={director}>{director}</li>);
+    const actors = film.actors.map((actor) => <li key={actor}>{actor}</li>);
+    const contributors = film.contributors.map((contrib) => <li key={contrib}>{contrib}</li>);
 
 
-    return(
+    return (
         <div className={styles.background}>
             <div className={styles.content}>
-            <img className = {styles.image} src={`/filmImages${film.poster_path}`} />
-                <div className = {styles.text}>
-                        <h3><strong>{film.title}</strong></h3>
-                        <hr></hr>
+                <img className={styles.image} src={`/filmImages${film.poster_path}`} />
+                <div className={styles.text}>
+                    <h3><strong>{film.title}</strong></h3>
+                    <hr />
                         <p>{film.overview}</p>
-                        <hr></hr>
-                        <div className={styles.contributorsContainer}>
-                            <div className={styles.directors}>
-                                <h3>Directors</h3>
-                                <ul>
-                                    {directors}
-                                </ul>
-                            </div>
-                            <div className={styles.actors}>
-                                <h3>Actors</h3>
-                                <ul>
-                                    {actors}
-                                </ul>
-                            </div>
-                            <div className={styles.contributors}>
-                                <h3>Contributors</h3>
-                                <ul>
-                                    {contributors}
-                                </ul>
+                    <hr />
+                    <div className={styles.contributorsContainer}>
+                        <div className={styles.directors}>
+                            <h3>Directors</h3>
+                            <ul>
+                                {directors}
+                            </ul>
+                        </div>
+                        <div className={styles.actors}>
+                            <h3>Actors</h3>
+                            <ul>
+                                {actors}
+                            </ul>
+                        </div>
+                        <div className={styles.contributors}>
+                            <h3>Contributors</h3>
+                            <ul>
+                                {contributors}
+                            </ul>
                         </div>
                     </div>
                 </div>
-            
             </div>
-            
-            
         </div>
+
+
     )
 }
