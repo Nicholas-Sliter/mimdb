638131585c5ebad86a715189d4ade869cb46c55f
xandershea <abuchinger@middlebury.edu>
2021-11-07 16:18:56 -0500

    changed the single film display module and css

--- src/components/SingleFilmDisplay.js
+++ src/components/SingleFilmDisplay.js
@@ -24,44 +24,41 @@ export default function SingleFilmDisplay({film}) {
     const actors = film.actors.map((actor)=><li>{actor}</li>);
     const contributors = film.contributors.map((contrib)=><li>{contrib}</li>);
 
+
     return(
         <div className={styles.background}>
-            <div className={styles.head}>
-                <div className={styles.titleContainer}>
-                    <h1 className={styles.title}>{film.title}</h1>
+            <div className={styles.content}>
+            <img src={`/filmImages${film.poster_path}`} />
+                <div className = {styles.text}>
+                        <h3><strong>{film.title}</strong></h3>
+                        <hr></hr>
+                        <p>{film.overview}</p>
+                        <hr></hr>
+                        <div className={styles.contributorsContainer}>
+                            <div className={styles.directors}>
+                                <h3>Directors</h3>
+                                <ul>
+                                    {directors}
+                                </ul>
+                            </div>
+                            <div className={styles.actors}>
+                                <h3>Actors</h3>
+                                <ul>
+                                    {actors}
+                                </ul>
+                            </div>
+                            <div className={styles.contributors}>
+                                <h3>Contributors</h3>
+                                <ul>
+                                    {contributors}
+                                </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.dateContainer}>
-                    <p className={styles.date}>{film.release_date}</p>
-                </div>
+            
             </div>
-            <div className={styles.filmPageBody}>
-                <div className={styles.filmContainer}>
-                    <img className={styles.video} src={`/filmImages${film.poster_path}`}/>
-                </div>
-                <div className={styles.overview}>
-                    <p className={styles.overviewText}>{film.overview}</p>
-                </div>
-                <div className={styles.contributorsContainer}>
-                    <div className={styles.directors}>
-                        <h3>Directors</h3>
-                        <ul>
-                            {directors}
-                        </ul>
-                    </div>
-                    <div className={styles.actors}>
-                        <h3>Actors</h3>
-                        <ul>
-                            {actors}
-                        </ul>
-                    </div>
-                    <div className={styles.contributors}>
-                        <h3>Contributors</h3>
-                        <ul>
-                            {contributors}
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            
+            
         </div>
     )
 }
