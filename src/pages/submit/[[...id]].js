a4c1ad5d1518f85fdeadd0db61c92e492f321eaf
kkosior1120 <kkosior@middlebury.edu>
2021-11-14 14:29:33 -0500

    fixed imports

--- src/pages/submit/[[...id]].js
+++ src/pages/submit/[[...id]].js
@@ -1,10 +1,11 @@
-import CustomHead from "../components/CustomHead";
-import Header from "../components/Header";
-import FilmRow from "../components/DisplayLayouts/FilmRow";
+import CustomHead from "../../components/CustomHead";
+import Header from "../../components/Header";
+import Submit from "../../components/Submit";
+//import FilmRow from "../../components/DisplayLayouts/FilmRow";
 
-import useFeatured from "../hooks/useFeatured";
+//import useFeatured from "../hooks/useFeatured";
 
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 export default function Home({films, genres, courses}) {
 
@@ -13,13 +14,7 @@ export default function Home({films, genres, courses}) {
       <CustomHead />
       <Header genreList={genres} classList={courses} />
       <main>
-
-        <FilmRow
-          displayType="wide"
-          films={useFeatured(films)}
-          title="Featured Films!"
-        />
-        <FilmRow films={films} title="All Films!" />
+        <Submit genres={genres} courses={courses}/>
       </main>
 
       <footer>
