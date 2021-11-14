0fc26faa99e960e1aac88a517099afa84b18c5d3
Nick McKalip <nmckalip@middlebury.edu>
2021-11-14 17:11:47 -0500

    Not working bit gap at bottom, Nicholas working on it so pausing for now

--- src/components/SingleFilmDisplay.js
+++ src/components/SingleFilmDisplay.js
@@ -5,6 +5,7 @@
 
 ******/
 
+import { urlObjectKeys } from "next/dist/shared/lib/utils";
 import styles from "../styles/SingleFilmDisplay.module.scss";
 
 
@@ -23,48 +24,53 @@ export default function SingleFilmDisplay({ film }) {
     const contributors = film.contributors.map((contrib) => <li key={contrib}>{contrib}</li>);
 
 
+    //style={`background-image: url(${film.backdrop_path})`}
+
     return (
-        <div className={styles.background}>
-            <div className={styles.content}>
-                <div className={styles.text}>
-                    <div className={styles.titleContainer}>
-                        <h3 className={styles.title}>
-                            <strong>{film.title}</strong>
-                        </h3>
-                        <h3 className={styles.date}>Release Date: {film.release_date}</h3>
-                    </div>
-                    <div className={styles.filmContainer}>
-                        <iframe className={styles.film} src="https://player.vimeo.com/video/644532680?h=89746a8c96&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1158" height="720" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="Sample Video for Website"/>
-                    </div>
-                    <div className={styles.overviewContainer}>
-                        <h3>OVERVIEW</h3>
-                        <div className={styles.overview}>
-                            <p>{film.overview}</p>
+        <div className={styles.backgroundIMG} style={{ backgroundImage: `url(/filmImages${film.backdrop_path})`, backgroundSize: "cover" }}>
+            <div className={styles.background}>
+                <div className={styles.content}>
+                    <div className={styles.text}>
+                        <div className={styles.titleContainer}>
+                            <h3 className={styles.title}>
+                                <strong>{film.title}</strong>
+                            </h3>
+                            <h3 className={styles.date}>Release Date: {film.release_date}</h3>
                         </div>
-                    </div>
-                    <div className={styles.contributorsContainer}>
-                        <div className={styles.directors}>
-                            <h3>Directors</h3>
-                            <ul>
-                                {directors}
-                            </ul>
+                        <div className={styles.filmContainer}>
+                            <iframe className={styles.film} src="https://player.vimeo.com/video/644532680?h=89746a8c96&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1158" height="720" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="Sample Video for Website" />
                         </div>
-                        <div className={styles.actors}>
-                            <h3>Actors</h3>
-                            <ul>
-                                {actors}
-                            </ul>
+                        <div className={styles.overviewContainer}>
+                            <h3>OVERVIEW</h3>
+                            <div className={styles.overview}>
+                                <p>{film.overview}</p>
+                            </div>
                         </div>
-                        <div className={styles.contributors}>
-                            <h3>Contributors</h3>
-                            <ul>
-                                {contributors}
-                            </ul>
+                        <div className={styles.contributorsContainer}>
+                            <div className={styles.directors}>
+                                <h3>Directors</h3>
+                                <ul>
+                                    {directors}
+                                </ul>
+                            </div>
+                            <div className={styles.actors}>
+                                <h3>Actors</h3>
+                                <ul>
+                                    {actors}
+                                </ul>
+                            </div>
+                            <div className={styles.contributors}>
+                                <h3>Contributors</h3>
+                                <ul>
+                                    {contributors}
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        
 
 
     )
