a076b12841adeb2003e051cb3aae2d1897335f34
kkosior1120 <kkosior@middlebury.edu>
2021-11-14 14:23:33 -0500

    added bare-boned submit page

---
+++ src/pages/submit/[[...id]].js
@@ -0,0 +1,30 @@
+import CustomHead from "../components/CustomHead";
+import Header from "../components/Header";
+import FilmRow from "../components/DisplayLayouts/FilmRow";
+
+import useFeatured from "../hooks/useFeatured";
+
+import styles from "../styles/Home.module.css";
+
+export default function Home({films, genres, courses}) {
+
+  return (
+    <div className={styles.container}>
+      <CustomHead />
+      <Header genreList={genres} classList={courses} />
+      <main>
+
+        <FilmRow
+          displayType="wide"
+          films={useFeatured(films)}
+          title="Featured Films!"
+        />
+        <FilmRow films={films} title="All Films!" />
+      </main>
+
+      <footer>
+         © {`${new Date().getFullYear()}`} Middlebury Movie Database
+      </footer>
+    </div>
+  );
+}
