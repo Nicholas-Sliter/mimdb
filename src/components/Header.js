abc693ea8fbe6bede145ef27fe3c43d108d5e12e
Nick McKalip <nmckalip@middlebury.edu>
2021-11-14 20:52:30 -0500

    Added a dynamic header, links do nothing, weird double press for true

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,24 @@ import styles from "../styles/NavBar.module.css";
 import Category from "./Category";
 import SearchBar from "./SearchBar";
 import Link from "next/link";
+import { useState } from "react";
 
 
 export default function Header({genreList, classList}) {
+  const [categoriesOpen, setCategoriesOpen] = useState();
+  let button = false;
+
+
+  const buttonFunc = () => {
+    if (button === true){
+      button = false
+    }
+    else{
+      button = true
+    }
+    setCategoriesOpen(button);
+  };
+
 
   return (
     <header>
@@ -16,9 +31,32 @@ export default function Header({genreList, classList}) {
           </a>
         </Link>
         <div className={styles.filter}>
-          <Category fieldName={"Genre"} fieldList={genreList} /> 
-          <Category fieldName={"Course"} fieldList={classList} />
-          <SearchBar />
+            <div className={styles.bigScreen}>
+              <div className={styles.fullWidthContainer}>
+                <Category fieldName={"Genre"} fieldList={genreList}/>
+                <Category fieldName={"Course"} fieldList={classList}/>
+                <SearchBar />
+              </div>
+            </div>
+
+
+            <div className={styles.smallScreen}>
+            <SearchBar />
+              <button className={styles.menuButton} onClick={() => buttonFunc()}>
+                <div/>
+                <div/>
+                <div/> 
+              </button>
+                <div className={categoriesOpen ? styles.dropDownContainerShow : styles.dropDownContainer}> 
+                  <ul className={styles.dropDownList}>
+                    <li><a>Filter</a></li>
+                    <li><a>Courses</a></li>
+                  </ul>
+
+                </div>
+            </div>
+            
+
         </div>
       </div>
     </header>
