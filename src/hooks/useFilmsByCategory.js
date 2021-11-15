ab892700df6601406abba97ab9b7d9a29e140aef
Wayne Wang <sw@middlebury.edu>
2021-11-15 09:26:30 -0500

    fixed according to comments

--- src/hooks/useFilmsByCategory.js
+++ src/hooks/useFilmsByCategory.js
@@ -4,14 +4,16 @@ export default function useFilmsByCategory({ category, value }) {
   const [categoryFilmList, setCategoryFilmList] = useState([]);
 
   useEffect(async () => {
-    if (value) {
-      const res = await fetch(`/api/films?${category}=${value}`);
-      if (!res.ok) {
-        throw new Error("Failed to fetch films");
-      }
-      const data = await res.json();
-      setCategoryFilmList(data);
+    if (!value) {
+      return;
     }
+    const res = await fetch(`/api/films?${category}=${value}`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch films");
+    }
+    const data = await res.json();
+    setCategoryFilmList(data);
   }, [category, value]);
+  
   return categoryFilmList;
 }
--- src/pages/api/films/index.js
+++ src/pages/api/films/index.js
@@ -17,21 +17,19 @@ const handler = nc().get(async (req, res) => {
 
   const filter = Object.keys(filters)[0];
   const value = filters[filter];
-  let id_list = [];
-  if (filter === "genre") {
-    id_list = await getFilmsByGenre(value);
-  } else if (filter === "course") {
-    id_list = await getFilmsByCourse(value);
-  } else if (filter === "language") {
-    id_list = await getFilmsByLanguage(value);
-  } else if (filter === "director") {
-    id_list = await getFilmsByDirector(value);
-  } else if (filter === "actor") {
-    id_list = await getFilmsByActor(value);
-  } else if (filter === "contributor") {
-    id_list = await getFilmsByContributor(value);
-  }
 
+  const func = {
+    "genre" : getFilmsByGenre,
+    "course" : getFilmsByCourse,
+    "language" : getFilmsByLanguage,
+    "director" : getFilmsByDirector,
+    "actor" : getFilmsByActor,
+    "contributor" : getFilmsByContributor,
+  };
+  
+  // having its format as [{film_id: 3}, {film_id:52}, ...]
+  const id_list = await func[filter](value);
+  
   const result = await Promise.all(id_list.map(async (film_id) => await getFilmById(film_id.film_id)));
   res.status(200).json(result);
 });
