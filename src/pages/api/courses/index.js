60382aa7431588fc9f31814f946a4f2da658903a
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-11-15 20:40:06 -0500

    Added validation and error status codes to courses

--- src/pages/api/courses/index.js
+++ src/pages/api/courses/index.js
@@ -5,6 +5,14 @@ import { getAllCourses } from "../../../lib/backend-utils";
 
 const handler = nc().get(async (req, res) => {
   const allCourses = await getAllCourses();
+
+  if (!allCourses || !Array.isArray(allCourses)) {
+    res.status(500).json({
+      error: "Could not retrieve courses",
+    });
+    return;
+  }
+
   res.status(200).json(allCourses);
 
 });
