5543ce9db08995bcec2da1020fa5ba412eea3082
Jiaqi-beep <jiaqili1620@gmail.com>
2021-11-16 21:43:15 -0500

    add divs

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -13,87 +13,100 @@ export default function Submit({ genres, courses }) {
     return (
         <div className={styles.submitPage}>
             <form>
-                <label htmlFor="title"> Title: </label>
-                <input
-                    type="text"
-                    id="title"
-                    placeholder="Title must be set"
-                    onChange={(event) => setTitle(event.target.value)}
-                />
-                <br /><br />
-                <label htmlFor="logLine"> Log-Line: </label>
-                <input
-                    type="text"
-                    id="logLine"
-                    placeholder="Log-Line must be set"
-                    onChange={(event) => setLogLine(event.target.value)}
-                />
-                <br /><br />
-                <label htmlFor="overview"> Overview: </label><br />
-                <textarea className={styles.textarea}
-                    id="overview"
-                    placeholder="Overview of Film"
-                    onChange={(event) => setOverview(event.target.value)}
+                <div className={styles.column}>
+                    <label htmlFor="title"> Title: </label>
+                    <input
+                        type="text"
+                        id="title"
+                        placeholder="Title must be set"
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                </div>
+                <div className={styles.column}>
+                    <label htmlFor="logLine"> Log-Line: </label>
+                    <input
+                        type="text"
+                        id="logLine"
+                        placeholder="Log-Line must be set"
+                        onChange={(event) => setLogLine(event.target.value)}
+                    />
+                </div>
 
-                />
                 <br /><br />
-                <label htmlFor="genres"> Genre: </label>
-                <div id="genres">
-                    {genres.map((genre) =>
-                        <div key={genre}>
-                            <input
-                                type="checkbox"
-                                id={genre}
-                                name={genre}
-                                value={genre}
-                            />
-                            <label htmlFor={genre}>{genre}</label><br />
-                        </div>
-                    )}
-                    <input
-                        type="checkbox"
-                        id="other1"
-                        name="other1"
-                        value="other1"
-                        onClick={() => addGenre(!newGenre)}
+
+                <div>
+                    <label htmlFor="overview"> Overview: </label><br />
+                    <textarea className={styles.textarea}
+                        id="overview"
+                        placeholder="Overview of Film"
+                        onChange={(event) => setOverview(event.target.value)}
                     />
-                    <label htmlFor="other1">Other</label>
-                    {newGenre ? <input
-                        type="text"
-                        id="otherText1"
-                        placeholder="Other"
-                    />
-                        : <div> </div>}
                 </div>
+                
                 <br /><br />
-                <label htmlFor="courses"> Course: </label>
-                <div id="courses">
-                    {courses.map((course) =>
-                        <div key={course}>
-                            <input
-                                type="checkbox"
-                                id={course}
-                                name={course}
-                                value={course}
-                            />
-                            <label htmlFor={course}>{course}</label><br />
-                        </div>
-                    )}
-                    <input
-                        type="checkbox"
-                        id="other2"
-                        name="other2"
-                        value="other2"
-                        onClick={() => addCourse(!newCourse)}
-                    />
-                    <label htmlFor="other2">Other</label>
-                    {newCourse ? <input
-                        type="text"
-                        id="otherText2"
-                        placeholder="Other"
-                    />
-                        : <div> </div>}
+
+                <div className={styles.columnlist}>
+                    <label htmlFor="genres"> Genre: </label>
+                    <div id="genres">
+                        {genres.map((genre) =>
+                            <div key={genre}>
+                                <input
+                                    type="checkbox"
+                                    id={genre}
+                                    name={genre}
+                                    value={genre}
+                                />
+                                <label htmlFor={genre}>{genre}</label><br />
+                            </div>
+                        )}
+                        <input
+                            type="checkbox"
+                            id="other1"
+                            name="other1"
+                            value="other1"
+                            onClick={() => addGenre(!newGenre)}
+                        />
+                        <label htmlFor="other1">Other</label>
+                        {newGenre ? <input
+                            type="text"
+                            id="otherText1"
+                            placeholder="Other"
+                        />
+                            : <div> </div>}
+                    </div>
                 </div>
+
+                <div className={styles.columnlist}>
+                    <label htmlFor="courses"> Course: </label>
+                    <div id="courses">
+                        {courses.map((course) =>
+                            <div key={course}>
+                                <input
+                                    type="checkbox"
+                                    id={course}
+                                    name={course}
+                                    value={course}
+                                />
+                                <label htmlFor={course}>{course}</label><br />
+                            </div>
+                        )}
+                        <input
+                            type="checkbox"
+                            id="other2"
+                            name="other2"
+                            value="other2"
+                            onClick={() => addCourse(!newCourse)}
+                        />
+                        <label htmlFor="other2">Other</label>
+                        {newCourse ? <input
+                            type="text"
+                            id="otherText2"
+                            placeholder="Other"
+                        />
+                            : <div></div>}
+                    </div>
+                </div>
+
                 <br /> <br />
                 <label htmlFor="directors"> Directors: </label><br />
                 <textarea
@@ -122,19 +135,23 @@ export default function Submit({ genres, courses }) {
                     cols="100"
                 />
                 <br /><br />
-                <label htmlFor="semester"> Semester released: </label>
-                <input
-                    type="text"
-                    id="semester"
-                    placeholder="eg. F21, W22, S22, etc."
-                />
-                <br /><br />
-                <label htmlFor="duration"> Duration: </label>
-                <input
-                    type="number"
-                    id="duration"
-                    placeholder="minutes"
-                />
+
+                <div className={styles.column}>
+                    <label htmlFor="semester"> Semester released: </label>
+                    <input
+                        type="text"
+                        id="semester"
+                        placeholder="eg. F21, W22, S22, etc."
+                    />
+                </div>
+                <div className={styles.column}>
+                    <label htmlFor="duration"> Duration: </label>
+                    <input
+                        type="number"
+                        id="duration"
+                        placeholder="minutes"
+                    />
+                </div>
                 <br /><br />
 
                 <input
