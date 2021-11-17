732491d8c9a06028f4271f30900244ec811298e2
Nick McKalip <nmckalip@middlebury.edu>
2021-11-17 15:51:54 -0500

    Fixed styling, vimeo is embedded iframe not npm

--- src/components/SingleFilmDisplay.js
+++ src/components/SingleFilmDisplay.js
@@ -10,6 +10,8 @@ import styles from "../styles/SingleFilmDisplay.module.scss";
 
 import data from "../../data/tempData.json";
 
+import Link from "next/link";
+
 
 export default function SingleFilmDisplay({ film }) {
 
@@ -23,6 +25,7 @@ export default function SingleFilmDisplay({ film }) {
     const directors = film.directors.map((director) => <li key={director}>{director}</li>);
     const actors = film.actors.map((actor) => <li key={actor}>{actor}</li>);
     const contributors = film.contributors.map((contrib) => <li key={contrib}>{contrib}</li>);
+    const course = film.course;
 
 
     //While images are not stored in Database
@@ -45,42 +48,53 @@ export default function SingleFilmDisplay({ film }) {
                             </h3>
                             <h3 className={styles.date}>Release Date: {film.release_date}</h3>
                         </div>
-                        <div className={styles.filmContainer}>
-                            <iframe className={styles.film} src="https://player.vimeo.com/video/644532680?h=89746a8c96&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1158" height="720" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="Sample Video for Website" />
-                        </div>
-                        <div className={styles.overviewContainer}>
-                            <h3>OVERVIEW</h3>
-                            <div className={styles.overview}>
-                                <p>{film.overview}</p>
+                        <div className={styles.largeColumnL}>
+
+                            <div className={styles.filmContainer}>
+                                <iframe className={styles.film} src="https://player.vimeo.com/video/644532680?h=89746a8c96&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="1158" height="720" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen title="Sample Video for Website" />
+                            </div>
+                            <div className={styles.overviewContainer}>
+                                <h3>OVERVIEW</h3>
+                                <div className={styles.overview}>
+                                    <p>{film.overview}</p>
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.courseContainer}>
-                            <h3>Course</h3>
-                            <div className={styles.overview}>
-                                <h4>{film.course}</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        
+                        <div className={styles.largeColumnR}>
+                            
+                            <div className={styles.courseContainer}>
+                                <h3>Course</h3>
+                                <div className={styles.overview}>
+                                    <Link href={`/films/course/${encodeURIComponent(course)}`} passHref key="course">
+                                        <h4 className={styles.courseName}>{course}</h4>
+                                    </Link>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div>
+                            </div>
+                            <div className={styles.contributorsContainer}>
+                                <div className={styles.directors}>
+                                    <h3>Directors</h3>
+                                    <ul>
+                                        {directors}
+                                    </ul>
+                                </div>
+                                <div className={styles.actors}>
+                                    <h3>Actors</h3>
+                                    <ul>
+                                        {actors}
+                                    </ul>
+                                </div>
+                                <div className={styles.contributors}>
+                                    <h3>Contributors</h3>
+                                    <ul>
+                                        {contributors}
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.contributorsContainer}>
-                            <div className={styles.directors}>
-                                <h3>Directors</h3>
-                                <ul>
-                                    {directors}
-                                </ul>
-                            </div>
-                            <div className={styles.actors}>
-                                <h3>Actors</h3>
-                                <ul>
-                                    {actors}
-                                </ul>
-                            </div>
-                            <div className={styles.contributors}>
-                                <h3>Contributors</h3>
-                                <ul>
-                                    {contributors}
-                                </ul>
-                            </div>
-                        </div>
+
+
                     </div>
                 </div>
             </div>
