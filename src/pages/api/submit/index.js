bd11899b6f03c336428e3caee227ff835c4464ef
Wayne Wang <sw@middlebury.edu>
2021-11-17 23:14:23 -0500

    fixed bugs

--- src/pages/api/submit/index.js
+++ src/pages/api/submit/index.js
@@ -1,13 +1,21 @@
 import nc from "next-connect";
 import { addFilm, getFilmBySlug } from "../../../lib/backend-utils";
-import { onError } from "../../../lib/middleware";
 import { convertToSlug } from "../../../lib/frontend-utils";
 
 // Validates the inFilm object and add default empty picture paths
 // TODO: potentially in the future if we have time, we can develop the validation process into a smart match process.
 const validateAndProcessNewFilm = async (inFilm) => {
   try {
-    let processedFilm = {...inFilm, slug: convertToSlug(processedFilm.title)};
+    // let processedFilm = {...inFilm, slug: convertToSlug(processedFilm.title)};
+    let processedFilm = {
+      "overview": inFilm.overview,
+      "description": inFilm.description,
+      "release_date": inFilm.release_date,
+      "title": inFilm.title,
+      "vimeo_id": inFilm.vimeo_id,
+      "duration": inFilm.duration,
+      slug: convertToSlug(inFilm.title)
+    }
 
     // check slug, increment if duplicates slug
     let index = 0;
@@ -17,8 +25,8 @@ const validateAndProcessNewFilm = async (inFilm) => {
     }
     
     // Add default empty picture paths
-    processedFilm.backdrop_path = "";
-    processedFilm.poster_path = "";
+    processedFilm.backdrop_path = "/sp_backdrop.jpg";
+    processedFilm.poster_path = "/sp_poster.jpg";
     
     // Generate vimeo boolean, simple
     processedFilm.video = processedFilm.vimeo_id && processedFilm.vimeo_id!="12345678";
@@ -33,15 +41,14 @@ const validateAndProcessNewFilm = async (inFilm) => {
 };
 
 
-const handler = nc({ onError })
+const handler = nc()
   .post(async (req, res) => {
     const newFilm = req.body;
 
     const processedFilm = await validateAndProcessNewFilm(newFilm);
 
     if (processedFilm) {
-      await addFilm(processedFilm);
-      res.status(200).json(processedFilm);
+      res.status(200).json(await addFilm(processedFilm));
     } else {
       res.status(500).json({
         error: "New film validation did not pass"
