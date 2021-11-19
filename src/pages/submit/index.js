ccb9160e3171f3d04c165336ea3d3513b315f578
Jiaqi-beep <jiaqili1620@gmail.com>
2021-11-19 10:30:23 -0500

    change font sizes

--- src/pages/submit/index.js
+++ src/pages/submit/index.js
@@ -2,10 +2,12 @@ import CustomHead from "../../components/CustomHead";
 import Header from "../../components/Header";
 import Submit from "../../components/Submit";
 
+import styles from "../../styles/Home.module.css";
+
 export default function Home({genres, courses}) {
 
   return (
-    <div /*className={styles.container}*/>
+    <div className={styles.container}>
       <CustomHead />
       <Header genreList={genres} classList={courses} />
       <main>
