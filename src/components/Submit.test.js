640709d94239e5e4d0e3859360c2085f7c1b8954
Jiaqi-beep <jiaqili1620@gmail.com>
2021-11-21 18:07:54 -0500

    init test

--- src/components/Submit.test.js
+++ src/components/Submit.test.js
@@ -2,19 +2,39 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import Submit from "./Submit";
 
 describe("Submit: Submiter tests", () => {
-  let article;
-  const handler = jest.fn();
+    let article;
+    const handler = jest.fn();
 
-  beforeEach(() => {
-    film = {
-      title: "Title of sample film",
-      logLine: "LogLine of the sample film",
-      overview: "Overview of the sample film",
-      
-    };
+    describe("Submit", () => {
+        beforeEach(() => {
+            film = {
+                title: "sample",
+                logLine: "LogLine of the sample film",
+                release_date: "F21",
+                duration: "90",
+                courseId: "CSCI0312",
+                vimeoId: "vimeoId",
+                overview: "Overview of the sample film"
+            };
 
-    handler.mockReset();
-  });
+            handler.mockReset();
+        });
+
+        // mock titles route
+        fetchMock.get(`/api/film/sample`, (url) => {
+            const section = url.charAt(url.length - 1);
+            const currentArticles = data.filter(
+                (article) => article.title.charAt(0) === section
+            );
+
+            return currentArticles.map((article) => ({
+                title: article.title,
+                id: article.id,
+            }));
+        });
+    })
+
+
 
 
 })
