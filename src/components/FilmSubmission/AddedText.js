f0c68475c1d19c0845b7b5acbdf755b3141f19d5
Jiaqi-beep <jiaqili1620@gmail.com>
2021-11-22 15:58:43 -0500

    fix addedtext key

--- src/components/FilmSubmission/AddedText.js
+++ src/components/FilmSubmission/AddedText.js
@@ -15,14 +15,13 @@ export default function AddedText({ name, inputList, setInputList }) {
         setInputList([...inputList, ""]);
     };
 
-    let key = 0;
 
     return (
         <div className={styles.smallInput}>
             <label htmlFor={`${name} 1`}> {`${name}s`}</label>
             <div className={styles.small}>
                 {inputList.map((i, n) =>
-                    <TextInput key={key++} name={`${name} ${n + 1}`} setFunc={addInput} moreText={`${name} ${n + 1}`} />
+                    <TextInput key={`${name} ${n + 1}`} name={`${name} ${n + 1}`} setFunc={addInput} moreText={`${name} ${n + 1}`} />
                 )}
             </div>
             <button onClick={() => handleAddClick()}>{`Add ${name}`}</button>
