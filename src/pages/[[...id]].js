e05d84e12d8fa3640d5aa93744e05017fb5751a4
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-11-24 12:06:26 -0500

    added layout component example to the main index page

--- src/pages/[[...id]].js
+++ src/pages/[[...id]].js
@@ -2,38 +2,33 @@ import CustomHead from "../components/CustomHead";
 import Header from "../components/Header";
 import FilmRow from "../components/DisplayLayouts/FilmRow";
 import FilmController from "../components/DisplayLayouts/FilmController";
+import Layout from "../components/Layouts/Layout"
 
 import useFeatured from "../hooks/useFeatured";
 
 import styles from "../styles/Home.module.css";
 
-export default function Home({ films }) {
+//need to remove films here and modify useFeatured
+export default function Home({films}) {
   return (
-    <div className={styles.container}>
-      <CustomHead />
-      <Header/>
-      <main>
-        <FilmRow
-          displayType="wide"
-          films={useFeatured(films)}
-          title="Featured Films!"
-        />
-        <FilmRow films={films} title="All Films! (No wrap)" wrap={false} />
-        <FilmController
-          title="Dramas"
-          queryObj={{ genre: "Drama", course: "" }}
-          rowStyleObject={{ displayType: "small", wrap: true }}
-        />
-        <FilmController
-          title="Sci-Fi"
-          queryObj={{ genre: "Sci-fi", course: "" }}
-          rowStyleObject={{ displayType: "small", wrap: true }}
-        />
-      </main>
-
-      <footer>
-        © {`${new Date().getFullYear()}`} Middlebury Movie Database
-      </footer>
-    </div>
+    <Layout>
+      <FilmRow
+        displayType="wide"
+        films={useFeatured(films)}
+        title="Featured Films!"
+      />
+      <FilmRow films={films} title="All Films! (No wrap)" wrap={false} />
+      <FilmController
+        title="Dramas"
+        queryObj={{ genre: "Drama", course: "" }}
+        rowStyleObject={{ displayType: "small", wrap: true }}
+      />
+      <FilmController
+        title="Sci-Fi"
+        queryObj={{ genre: "Sci-fi", course: "" }}
+        rowStyleObject={{ displayType: "small", wrap: true }}
+      />
+    </Layout>
   );
+  
 }
