87deb855f62e48f3e633e67b3e9ec78c09165799
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-11-30 13:04:09 -0500

    fixed lint

--- src/pages/[[...id]].js
+++ src/pages/[[...id]].js
@@ -1,12 +1,9 @@
-import CustomHead from "../components/CustomHead";
-import Header from "../components/Header";
 import FilmRow from "../components/DisplayLayouts/FilmRow";
 import FilmController from "../components/DisplayLayouts/FilmController";
 import Layout from "../components/Layouts/Layout"
 
 import useFeatured from "../hooks/useFeatured";
 
-import styles from "../styles/Home.module.css";
 
 //need to remove films here and modify useFeatured
 export default function Home({films}) {
