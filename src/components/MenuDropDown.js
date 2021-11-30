09639703f43c6c0bc382bf2e643a0018033568f7
Rebecca Hochman-Fisher <rhochmanfisher@middlebury.edu>
2021-11-30 13:25:39 -0500

    Changed mobile drop down to the same categories / functionality as desktop view

--- src/components/MenuDropDown.js
+++ src/components/MenuDropDown.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import Category from "./Category";
+import { useContext } from "react";
+import { GenreCourseContext} from  "./context/GenreCourseContext";
 
 export default function MobileMenuDropDown({ styleCont, styleList }) {
+  const {genres, courses} = useContext(GenreCourseContext);
   // TODO: we need to create dropdown components for the filter and courses
   // perhaps on click replace the current contents of this menu with the 
   // contents for the Filter and Course dropdowns
@@ -9,14 +13,10 @@ export default function MobileMenuDropDown({ styleCont, styleList }) {
     <div className={styleCont}>
       <ul className={styleList}>
         <li>
-          <Link href={`/search?keyword=`} passHref>
-            <a>Filter</a>
-          </Link>
+        <Category fieldName={"Genre"} fieldList={genres}/>
         </li>
         <li>
-          <Link href={`/films/course/`} passHref>
-            <a>Courses</a>
-          </Link>
+        <Category fieldName={"Course"} fieldList={courses}/>
         </li>
       </ul>
     </div>
