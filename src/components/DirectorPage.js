a6513ac36e97975d7625217dde7a1780db0c27c6
Nick McKalip <nmckalip@middlebury.edu>
2021-12-02 16:06:52 -0500

    Accidently didn't branch :(

--- src/components/DirectorPage.js
+++ src/components/DirectorPage.js
@@ -11,20 +11,20 @@ export default function DirectorPage({ films, director }) {
                 <div className={styles.directorInfo}>
                     <h1>{directorInfo.director_name}</h1>
                     <div className={styles.classEmail}>
-                        <h4 className={styles.classYear} testId="directorPageName">{directorInfo.director_graduation_year}</h4>
-                        <h4 className={styles.email} testId="directorMiddEmail">{directorInfo.director_midd_email}</h4>
-                        <h4 className={styles.email} testId="directorPerseEmail">{directorInfo.director_personal_email}</h4>
+                        <h4 className={styles.classYear}>{directorInfo.director_graduation_year}</h4>
+                        <h4 className={styles.email}>{directorInfo.director_midd_email}</h4>
+                        <h4 className={styles.email}>{directorInfo.director_personal_email}</h4>
                     </div>
                 </div>
                 <div className={styles.bio}>
                     <h3 >Biography</h3>
-                    <p className={styles.bioText} testId="directorBio">
+                    <p className={styles.bioText}>
                         {directorInfo.director_bio}
                     </p>
                 </div>
             </div>
             <div className={styles.filmRowCont}>
-                <FilmRow films={films} title={`Films by ${directorInfo.director_name}`} />
+                (<FilmRow films={films} title={`Films by ${directorInfo.director_name}`} />
             </div>
         </div>
     )
