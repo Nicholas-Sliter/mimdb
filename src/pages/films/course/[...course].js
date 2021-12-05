44587c4f46616d387fbc9c7591b970cbaaf22dc4
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-12-05 11:25:34 -0500

    fixed spacing on film course page

--- src/pages/films/course/[...course].js
+++ src/pages/films/course/[...course].js
@@ -1,10 +1,11 @@
-import { useRouter } from "next/router"
-import FilmRow from "../../../components/DisplayLayouts/FilmRow"
+import { useRouter } from "next/router";
+import FilmRow from "../../../components/DisplayLayouts/FilmRow";
 import Layout from "../../../components/Layouts/Layout";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-//import decodeURIComponentSafe from "../../../lib/frontend-utils"
+import { decodeURIComponentSafe } from "../../../lib/frontend-utils";
 
+import styles from "/src/styles/FilmCourse.module.scss";
 
 export default function FilmsByCourse() {
   const router = useRouter();
@@ -16,7 +17,7 @@ export default function FilmsByCourse() {
   const [courseDesc, setCourseDesc] = useState();
 
   //convert from uri encoding to text
-  const courseName = decodeURIComponent(course);
+  const courseName = decodeURIComponentSafe(course);
 
   useEffect(async () => {
     const res = await fetch(`/api/films?course=${course}`);
@@ -42,9 +43,11 @@ export default function FilmsByCourse() {
 
   return (
     <Layout pageTitle={titleString}>
-      <div className="container">
+      <div className={styles.courseContainer}>
         <h1>Films created in {courseName}</h1>
-        <h3>{courseDesc}</h3>
+        <p>{courseDesc}</p>
+      </div>
+      <div className={styles.container}>
         <FilmRow films={genreFilmList} />
       </div>
     </Layout>
