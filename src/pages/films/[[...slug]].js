0cae2aa3e45f56839c7da53e27936a3940180b00
Nicholas-Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2021-12-06 10:53:30 -0500

    HOTFIX: Style error in single film page

--- src/pages/films/[[...slug]].js
+++ src/pages/films/[[...slug]].js
@@ -11,9 +11,7 @@ export default function Film() {
 
     return (
         <Layout>
-        <div className={styles.container}>
             <SingleFilmDisplay film={useGetFilm(slug)}/>
-        </div>
         </Layout>
     )
 }
