b0dbea4035654022744d4dbaa3dfbead6760e622
Jiaqi-beep <jiaqili1620@gmail.com>
2021-12-06 13:51:31 -0500

    change update.js into reject and approve.js

--- src/pages/api/films/[slug]/approve.js
+++ src/pages/api/films/[slug]/approve.js
@@ -6,21 +6,21 @@ import { updateFilmApproval } from "../../../../lib/backend-utils";
 
 const handler = nc().put( async (req, res) => {
     const { slug } = req.query;
-    const update = JSON.parse(req.body).approval;
-    if (!update && update !== 0 && update !== "0"){
-      res.status(400);
-      return;
-    }
-    const success = await updateFilmApproval(slug, update);
+    // const update = JSON.parse(req.body).approval;
+    // if (!update && update !== 0 && update !== "0"){
+    //   res.status(400);
+    //   return;
+    // }
+    const success = await updateFilmApproval(slug, true);
 
     if (success) {
       res.status(200).json({
-        message: "Successfully updated film approval",
-        update: update
+        message: "Successfully approved film",
+        update: true
       })
     } else {
       res.status(500).json({
-        error: "Could not update film approval"
+        error: "Could not approve film"
       });
     }
   });
---
+++ src/pages/api/films/[slug]/reject.js
@@ -0,0 +1,28 @@
+// Approve API - update film approval
+
+import nc from "next-connect";
+import { updateFilmApproval } from "../../../../lib/backend-utils";
+
+
+const handler = nc().put( async (req, res) => {
+    const { slug } = req.query;
+    // const update = JSON.parse(req.body).approval;
+    // if (!update && update !== 0 && update !== "0"){
+    //   res.status(400);
+    //   return;
+    // }
+    const success = await updateFilmApproval(slug, false);
+
+    if (success) {
+      res.status(200).json({
+        message: "Successfully rejected film",
+        update: false
+      })
+    } else {
+      res.status(500).json({
+        error: "Could not reject film"
+      });
+    }
+  });
+  
+  export default handler;
