5c9d44f78ba317acfb617836577f1dedc3ec2b34
Wayne Wang <49931122+waynew99@users.noreply.github.com>
2021-12-06 09:59:18 -0500

    quick fix lint

--- src/pages/[[...id]].js
+++ src/pages/[[...id]].js
@@ -11,7 +11,7 @@ import useFeatured from "../hooks/useFeatured";
 import styles from "../styles/Home.module.css";
 
 //need to remove films here and modify useFeatured
-export default function Home({}) {
+export default function Home() {
 
   const discover = useContext(DiscoverContext);
 
