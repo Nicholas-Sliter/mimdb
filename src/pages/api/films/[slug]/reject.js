420002be4c6740745bec4a7d85ae577269afd8ea
kkosior1120 <kkosior@middlebury.edu>
2021-12-07 19:02:28 -0500

    etc

--- src/pages/api/films/[slug]/reject.js
+++ src/pages/api/films/[slug]/reject.js
@@ -7,20 +7,16 @@ import { updateFilmApproval } from "../../../../lib/backend-utils";
 
 const handler = nc().put( async (req, res) => {
     const { slug } = req.query;
-    //console.log(req)
     const session = await getSession({ req });
-    console.log("if")
     if (!session) {
       res.status(403).json({
         message: "Only logged in administrator can reject film"
       })
       return;
     }
-    console.log("after if")
     
     const success = await updateFilmApproval(slug, false);
 
-    console.log("if2")
     if (success) {
       res.status(200).json({
         message: "Successfully rejected film",
@@ -31,7 +27,6 @@ const handler = nc().put( async (req, res) => {
         error: "Could not reject film"
       });
     }
-    console.log("after if2")
   });
   
   export default handler;
