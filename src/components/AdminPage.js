49d83e5e5f877cb748bb77c788b6614a42a627bb
kkosior1120 <kkosior@middlebury.edu>
2021-12-07 19:03:41 -0500

    lint error

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function AdminPage({ films, adminFunc }) {
     const [update, refresh] = useState(true);
