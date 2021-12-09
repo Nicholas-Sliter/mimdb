49621e71d9d04522e3c515498359bc628b61aff2
Wayne Wang <49931122+waynew99@users.noreply.github.com>
2021-12-08 22:32:16 -0500

    Revert "Revert "Image selector""

---
+++ src/components/FilmSubmission/ImageSelector.js
@@ -0,0 +1,24 @@
+// Select an image from a list of pre-defined images
+
+import styles from '../../styles/components/ImageSelector.module.scss';
+
+export default function ImageSelector({images, selectedImage, onImageSelect}) {
+
+  return (
+    <div className={styles.imageSelector}>
+      {images.map((image) => {
+        const imageClass = (image !== selectedImage) ? styles.image : styles.image + " " + styles.selected;
+        const imagePartial = image.split(".")[0].split("/") ?? [""];
+        const imageName = imagePartial[imagePartial.length - 1] ?? "";
+        
+        //get the image alt from the name
+        return (
+            <img src={image} alt={image} title={imageName} key={image}
+            className={imageClass}
+            onClick={() => onImageSelect(image)}>
+          </img>
+        );
+      })}
+    </div>
+  );
+}
